a47b34a2299791136c2dd3ddf6c7d6b6e6a6001d
BlackSamorez <andrei@panferov-sci.ru>
2021-05-06 16:23:56 +0300

    Dataclass for implementations and fixed evolution

--- GRAPE.py
+++ GRAPE.py
@@ -2,22 +2,23 @@ import numpy as np
 import math
 import random
 from qiskit import QuantumCircuit
+from dataclasses import dataclass
 
 
 class BasicGate:  # 1-qubit gate
     def __init__(self, params=None):
         if params is None:
             params = [0, 0]
-        self.params = params
+        self.params: list[float] = params
         # Pauli matrices
-        self.I = np.eye(2, dtype=complex)
-        self.X = np.asarray([[0, 1], [1, 0]], dtype=complex)
-        self.Y = np.asarray([[0, -1j], [1j, 0]], dtype=complex)
+        self.__Id = np.eye(2, dtype=complex)
+        self.__X = np.asarray([[0, 1], [1, 0]], dtype=complex)
+        self.__Y = np.asarray([[0, -1j], [1j, 0]], dtype=complex)
 
     @property
     def matrix(self):  # straightforward matrix representation
-        matrix = math.cos(self.params[0] / 2) * self.I - 1j * math.sin(self.params[0] / 2) * (
-                    math.cos(self.params[1]) * self.X + math.sin(self.params[1]) * self.Y)
+        matrix = math.cos(self.params[0] / 2) * self.__Id - 1j * math.sin(self.params[0] / 2) * (
+                    math.cos(self.params[1]) * self.__X + math.sin(self.params[1]) * self.__Y)
 
         return matrix
 
@@ -30,36 +31,39 @@ class BasicGate:  # 1-qubit gate
 
 
 class EvolutionStep:  # a combination of 1-qubit operations followed by evolution
-    def __init__(self, size=2, params=None):
+    def __init__(self, size: int = 2, params=None, time: float = 0.2):
         if params is None:
             params = []
-        self.size = size  # number of qubits
-        self.time = 0.2  # evolution time
+        self.size: int = size  # number of qubits
+        self.time: float = time  # evolution time
 
         if len(params) != self.size:
             params += [[0, 0] for _ in range(self.size - len(params))]
 
         self.basicGates = [BasicGate(param) for param in params]  # 1-qubit gates
 
-        self.J = np.zeros((2 ** self.size, 2 ** self.size), dtype=complex)  # interaction matrix (NOT FINISHED)
+        self.J = np.zeros((self.size, self.size), dtype=complex)  # interaction matrix (NOT FINISHED)
         for i in range(self.size - 1):
-            self.J[i + 1][i] = 1  # костыль
+            self.J[i + 1][i] = 1
 
-        self.Z = np.asarray([[1, 0], [0, -1]], dtype=complex)
-        self.sigmas = [np.ones(1) for _ in range(self.size)]
-        for i in range(self.size):
-            for j in range(self.size):
-                if i == j:
-                    self.sigmas[j] = np.kron(self.sigmas[j], self.Z)
-                else:
-                    self.sigmas[j] = np.kron(self.sigmas[j], np.eye(2))
+        self.__Id = np.eye(2, dtype=complex)
+        self.__Z = np.asarray([[1, 0], [0, -1]], dtype=complex)
+
+    def sigmaz(self, i: int, j: int):
+        matrix = np.ones((1, 1), dtype=complex)
+        for k in range(self.size):
+            if k == i or k == j:
+                matrix = np.kron(matrix, self.__Z)
+            else:
+                matrix = np.kron(matrix, self.__Id)
+        return matrix
 
     @property
     def hamiltonian(self):
         hamiltonian = np.zeros((2 ** self.size, 2 ** self.size), dtype=complex)
         for i in range(self.size):
             for j in range(i):
-                hamiltonian += math.pi / 2 * self.J[i][j] * self.sigmas[i] @ self.sigmas[j]
+                hamiltonian += math.pi / 2 * self.J[i][j] * self.sigmaz(i, j)
         return hamiltonian
 
     @property
@@ -69,7 +73,7 @@ class EvolutionStep:  # a combination of 1-qubit operations followed by evolutio
             for j in range(i):
                 evolution = evolution @ (math.cos(math.pi / 2 * self.J[i][j] * self.time) * np.eye(2 ** self.size,
                                                                                                    dtype=complex) - 1j * math.sin(
-                    math.pi / 2 * self.J[i][j] * self.time) * self.sigmas[i] @ self.sigmas[j])
+                    math.pi / 2 * self.J[i][j] * self.time) * self.sigmaz(i, j))
         return evolution  # NOT FINISHED
 
     @property
@@ -101,25 +105,64 @@ class EvolutionStep:  # a combination of 1-qubit operations followed by evolutio
         return circuit
 
 
-class Implementation:  # class to approximate abstract unitary using a series of evolutionStep
-    def __init__(self, target, n):
+@dataclass()
+class Implementation:
+    name: str
+    angles: list[list[list[float, float]]]
+    times: list[float]
+    phase: float = 0
+
+    def save(self):
+        #                 qubit 1   ... ...   qubit k   time
+        # kick 1:       theta; phi; ... ... theta; phi; time;
+        # ...
+        # ...
+        # kick n:       theta; phi; ... ... theta; phi; time;
+        # global phase:    phase;   ... ...    phase;   phase;
+        #
+        file = open(self.name, "w")
+        for i in range(len(self.angles)):
+            for j in range(len(self.angles[0])):
+                file.write(f"{self.angles[i][j][0]}; {self.angles[i][j][1]}; ")
+            file.write(f"{self.times[i]}\n")
+        for i in range(2 * len(self.angles[0]) + 1):
+            file.write(f"{self.phase}")
+            if i != 2 * len(self.angles[0]):
+                file.write("; ")
+        file.write("\n")
+
+    def load(self, name):
+        data = np.genfromtxt(name, delimiter=";")
+        self.angles = [[[data[i][2 * j + 0], data[i][2 * j + 1]] for j in range(data.shape[1] - 1)] for i in range(data.shape[0])]
+        self.times = [data[i][-1] for i in range(data.shape[0])]
+        self.phase = data[-1][0]
+
+
+class Evolution:  # class to approximate abstract unitary using a series of evolutionStep
+    def __init__(self, target, n: int = 4, implementation: Implementation = None):
         self.target = target  # unitary to approximate
-        self.noise = 0.05  # noise levels (see self.stupidCalculateGradient)
+        self.noise: float = 0.05  # noise levels (see self.stupidCalculateGradient)
 
-        self.n = n  # number of evolution steps
-        self.size = int(math.log2(self.target.size) / 2)  # number of qubits
+        if implementation is not None:
+            if len(target) != (2 ** len(implementation.angles[0])):
+                print(target.size, (2 ** len(implementation.angles[0])))
+                raise Exception('Mismatch in target and implementation dimensions!')
+            self.__n = len(implementation.angles)
+            self.__size = len(implementation.angles[0])
+            self.phase = implementation.phase
+            self.gates = [EvolutionStep(size=self.__size, params=implementation.angles[i], time=implementation.times[i]) for i in range(self.__n)]
+        else:
+            self.__n: int = n  # number of evolution steps
+            self.__size = int(math.log2(self.target.size) / 2)  # number of qubits
 
-        self.phase = 0  # global phase
-        self.gates = [EvolutionStep(size=self.size) for _ in range(self.n)]  # evolution steps
+            self.phase = 0  # global phase
+            self.gates = [EvolutionStep(size=self.__size) for _ in range(self.__n)]  # evolution steps
 
-        self.evolutionGradient = [0] * self.n  # -gradient of cost function by evolution times
-        self.phaseGradient = 0  # -gradient of cost function by global phase
-        self.gradient = [[[0, 0] for _ in range(self.size)] for _ in
-                         range(self.n)]  # -gradient of cost function by 1-qubit operations parameters
+        self.evolutionGradient = [0] * self.__n  # -gradient of cost function by evolution times
+        self.gradient = [[[0, 0] for _ in range(self.__size)] for _ in
+                         range(self.__n)]  # -gradient of cost function by 1-qubit operations parameters
         self.stepSize = 0.01  # parameter update by gradient coefficient for 1-qubit operations parameters and
         # evolution times
-        self.phaseStepSize = 0.002  # parameter update by gradient coefficient for global phase (diverges when trying
-        # to lower)
 
     @property
     def time(self):  # total approximation time
@@ -134,47 +177,16 @@ class Implementation:  # class to approximate abstract unitary using a series of
 
     @property
     def matrix(self):  # approximation matrix
-        matrix = np.eye(2 ** self.size, dtype=complex)
+        matrix = np.eye(2 ** self.__size, dtype=complex)
 
-        for i in range(self.n):
+        for i in range(self.__n):
             matrix = self.gates[i].matrix @ matrix
 
         return matrix * math.e ** (1j * self.phase)
 
-    def write_params(self, params=None):  # writes params for 1-qubit operations
-        if params:
-            for i in range(self.n):
-                self.gates[i].params = params[i]
-        else:
-            for i in range(self.n):
-                self.gates[i].randomize_params()  # randomizes if no input
-
-    def save_params(self, filename):
-        # self.gates[0].basicGates[0].params[0], self.gates[0].basicGates[0].params[1]; ... ; self.gates[0].basicGates[self.size - 1].params[0]; self.gates[0].basicGates[self.size - 1].params[1]; self.gates[0].time
-        # ...
-        # self.gates[self.n - 1].basicGates[0].params[0], self.gates[0].basicGates[0].params[1]; ... ; self.gates[0].basicGates[self.size - 1].params[0]; self.gates[self.n - 1].basicGates[self.size - 1].params[1]; self.gates[self.n - 1].time
-        # self.phase; ... ; self.phase
-
-        file = open(filename, "w")
-        for i in range(self.n):
-            for j in range(self.size):
-                file.write(f"{self.gates[i].basicGates[j].params[0]}; {self.gates[i].basicGates[j].params[1]}; ")
-            file.write(f"{self.gates[i].time}\n")
-        for i in range(2 * self.size + 1):
-            file.write(f"{self.phase}")
-            if i != 2 * self.size:
-                file.write("; ")
-        file.write("\n")
-
-    def read_params(self, filename):
-        file = open(filename, "r")
-        data = np.genfromtxt(filename, delimiter=";")
-        for i in range(self.n):
-            for j in range(self.size):
-                self.gates[i].basicGates[j].params[0] = data[i][2 * j]
-                self.gates[i].basicGates[j].params[1] = data[i][2 * j + 1]
-            self.gates[i].time = data[i][2 * self.size]
-        self.phase = data[self.n][0]
+    def randomize_params(self, params=None):  # randomizes params for 1-qubit operations
+        for i in range(self.__n):
+            self.gates[i].randomize_params()
 
     def calculate_gradient(self):  # grad f = (f(x + gradstep) - f(x)) / gradstep
         dist1 = self.distance
@@ -182,8 +194,8 @@ class Implementation:  # class to approximate abstract unitary using a series of
         gradstep = 0.001
 
         # 1-qubit operations parameters gradient
-        for i in range(self.n):
-            for j in range(self.size):
+        for i in range(self.__n):
+            for j in range(self.__size):
                 for k in [0, 1]:
                     self.gates[i].basicGates[j].params[k] += gradstep
                     dist2 = self.distance
@@ -198,24 +210,17 @@ class Implementation:  # class to approximate abstract unitary using a series of
 
             self.evolutionGradient[i] = (dist1 - dist2) / gradstep * self.stepSize  # minus for descent
 
-        # global phase gradient
-        self.phase += gradstep
-        dist2 = self.distance
-        self.phase -= gradstep
-
-        self.phaseGradient += (dist1 - dist2) / gradstep * self.phaseStepSize  # minus for descent
-
         # make every step + random(-1, 1) * self.random
         if self.noise:
-            for i in range(self.n):
-                for j in range(self.size):
+            for i in range(self.__n):
+                for j in range(self.__size):
                     for k in [0, 1]:
                         self.gradient[i][j][k] += self.noise * 2 * (random.random() - 1) * self.gradient[i][j][k]
 
     def correct_params(self):  # update all parameters based on gradients
-        for i in range(self.n):
+        for i in range(self.__n):
             self.gates[i].correct_params(self.gradient[i], self.evolutionGradient[i])
-        self.phase = self.phaseGradient
+        self.phase -= np.angle((self.matrix @ self.target_d).trace())
 
     @property
     def distance(self):  # Frobenius norm
@@ -246,7 +251,12 @@ class Implementation:  # class to approximate abstract unitary using a series of
             return distances
 
     def to_qiskit(self):
-        circuit = QuantumCircuit(self.size, global_phase=self.phase)
-        for i in range(self.n):
+        circuit = QuantumCircuit(self.__size, global_phase=self.phase)
+        for i in range(self.__n):
             circuit += self.gates[i].to_qiskit()
         return circuit
+
+    def to_implementation(self, name: str):
+        angles = [[[gate.basicGates[i].params[0], gate.basicGates[i].params[1]] for i in range(self.__size)] for gate in self.gates]
+        times = [gate.time for gate in self.gates]
+        return Implementation(name, angles, times, phase=self.phase)
