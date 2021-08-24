85fbbf1e63acb2c69ac7a1ca9265da83d2a8c306
BlackSamorez <andrei@panferov-sci.ru>
2021-08-24 19:12:02 +0300

    test_all.py fixed

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,1 +1,10 @@
+import unittest
 
+from test_onequbitgates import *
+from test_multiqubitgates import *
+from test_circuit import *
+from test_GRAPE import *
+
+
+if __name__ == '__main__':
+    unittest.main()
