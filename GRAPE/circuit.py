NMROneQubitGate fixed. GradientOptimization fully working. Derivative test rewritten

GRAPE/circuit.py
@@ -1,10 +1,11 @@
 import numpy as np
-from .multiqubitgates import MultiQubitGate, Pulse
-from .onequbitgates import NMROneQubitGate
+
+from .multiqubitgates import MultiQubitGate, Pulse, Evolution
 
 
 class Circuit:
     """Class for representing quantum circuits as a series of quantum gates"""
+
     def __init__(self, size: int):
         self.size = size
         self.gates: list[MultiQubitGate] = []
@@ -60,8 +61,8 @@ class Circuit:
         if len(self) == 0:
             return ""
         string = ""
-        for i in range(len(self) - 1):
-            string += self.gates[i].__name__
+        for i in range(len(self)):
+            string += self.gates[i].__class__.__name__
             string += " "
         return string[:-1]
 
@@ -77,7 +78,8 @@ class Circuit:
             derivative_gate = self.gates[derivative_gate]
 
         if not issubclass(type(derivative_gate), MultiQubitGate):
-            raise TypeError(f"gate argument must be of int or MuliQubitGate type. {derivative_gate.__class__.__name__} was given")
+            raise TypeError(
+                f"gate argument must be of int or MuliQubitGate type. {derivative_gate.__class__.__name__} was given")
 
         derivative = np.eye(2 ** self.size, dtype=complex)
         for gate in self.gates:
@@ -89,13 +91,29 @@ class Circuit:
         return derivative
 
     def normalize(self):
+        """
+        Normalize circuit parameters
+        """
         for gate in self.gates:
             gate.normalize()
+
+    def set_hamiltonian(self, hamiltonian: np.ndarray):
+        """
+        Change hamiltonian of hamiltonian evolution gates
+        
+        :type hamiltonian: ndarray
+        :param hamiltonian: hamiltonian to be set
+        """
+        for gate in self.gates:
+            if type(gate) is Evolution:
+                gate.set_j(hamiltonian)
 
 
 class OneQubitEntanglementAlternation(Circuit):
     """Quantum circuit consisting of alternating single qubit rotations cascades and entanglement gates"""
-    def __init__(self, size: int, entanglement_gate_type: type(MultiQubitGate), number_of_entanglements: int):
+
+    def __init__(self, size: int, entanglement_gate_type: type(MultiQubitGate), number_of_entanglements: int,
+                 one_qubit_gate_type=None):
         super().__init__(size)
         assert issubclass(entanglement_gate_type, MultiQubitGate), "entanglement_gate_type must be a subclass of " \
                                                                    "MultiQubitGate "
@@ -105,4 +123,4 @@ class OneQubitEntanglementAlternation(Circuit):
         self.gates.append(Pulse(size))
         for i in range(number_of_entanglements):
             self.gates.append(entanglement_gate_type(size))
-            self.gates.append(Pulse(size, one_qubit_gate_type=NMROneQubitGate))
+            self.gates.append(Pulse(size, one_qubit_gate_type=one_qubit_gate_type))

GRAPE/multiqubitgates.py
@@ -2,8 +2,8 @@ import random
 from abc import ABC, abstractmethod
 
 import numpy as np
+
 from .onequbitgates import OneQubitGate, GeneralOneQubitGate, NMROneQubitGate
-from qiskit import QuantumCircuit
 
 
 class MultiQubitGate(ABC):
@@ -43,7 +43,7 @@ class MultiQubitGate(ABC):
         pass
 
 
-class Delay(MultiQubitGate):
+class Evolution(MultiQubitGate):
     """Evolution under fixed NMR Hamiltonian"""
 
     def __init__(self, size: int, time: float = 0):
@@ -57,7 +57,6 @@ class Delay(MultiQubitGate):
             for j in range(i + 1, self.size):
                 self.j[i][j] = 1
         self.hamiltonian = np.zeros((2 ** self.size, 2 ** self.size), dtype=complex)
-
         self.derivative = np.zeros((1, 2 ** self.size, 2 ** self.size), dtype=complex)
 
         class ParamsGetter:
@@ -114,15 +113,21 @@ class Delay(MultiQubitGate):
         self.update_matrix()
         self.update_derivative()
 
-    def set_j(self, new_j):
+    def set_j(self, new_j: np.ndarray):
         for i in range(self.j.shape[0]):
             for j in range(self.j.shape[1]):
                 self.j[i][j] = new_j[i][j]
 
     def randomize_params(self, exp=0.3):
-        self.time = random.uniform(0, exp / 0.00148)
+        if self.size == 3:
+            self.time = random.uniform(0, exp / 0.00148)
+        elif self.size == 2:
+            self.time = random.uniform(0, exp / 0.013)
+        else:
+            raise NotImplementedError("IDK")
 
     def to_circuit(self):
+        from qiskit import QuantumCircuit
         circuit = QuantumCircuit(self.size)
         circuit.hamiltonian(self.hamiltonian, float(self.time), circuit.qubits)
         return circuit
@@ -212,6 +217,7 @@ class Pulse(MultiQubitGate):
             basicGate.randomize_params()
 
     def to_circuit(self):
+        from qiskit import QuantumCircuit
         circuit = QuantumCircuit(self.size)
         for i in range(self.size):
             circuit.r(self.basic_gates[i].params[0], self.basic_gates[i].params[1], circuit.qubits[i])
@@ -272,6 +278,7 @@ class Inversion(MultiQubitGate):
         pass
 
     def to_circuit(self):
+        from qiskit import QuantumCircuit
         circuit = QuantumCircuit(self.size)
         for i in self.qubits:
             circuit.x(i)
@@ -342,6 +349,7 @@ class CXCascade(MultiQubitGate):
         pass
 
     def to_circuit(self):
+        from qiskit import QuantumCircuit
         circuit = QuantumCircuit(self.size)
         for i in range(self.size - 1):
             circuit.cx(i, i + 1)

GRAPE/onequbitgates.py
@@ -20,7 +20,8 @@ class OneQubitGate(ABC):
         if params is None:
             params = np.zeros(self.number_of_parameters, dtype=float)
         self.params: np.ndarray = np.asarray(params, dtype=float)
-        assert self.params.shape == (self.number_of_parameters,), f"Mismatch in number_of_params and shape of params provided: {self.number_of_parameters} vs {self.params.shape[0]} "
+        assert self.params.shape == (
+            self.number_of_parameters,), f"Mismatch in number_of_params and shape of params provided: {self.number_of_parameters} vs {self.params.shape[0]} "
 
     @abstractmethod
     def update_matrix(self):
@@ -45,6 +46,7 @@ class OneQubitGate(ABC):
 
 class GeneralOneQubitGate(OneQubitGate):
     """Representation of a general one qubit gate"""
+
     def __init__(self, params=None):
         super().__init__(3, params=params)  # params: theta, phi, lambda
 
@@ -75,7 +77,8 @@ class GeneralOneQubitGate(OneQubitGate):
         self.update_derivative()
 
     def randomize_params(self):
-        self.params = np.asarray([2 * np.pi * random.random(), 2 * np.pi * random.random(), 2 * np.pi * random.random()], dtype=float)
+        self.params = np.asarray(
+            [2 * np.pi * random.random(), 2 * np.pi * random.random(), 2 * np.pi * random.random()], dtype=float)
 
     def normalize(self):
         self.params[0] = self.params[0] % (4 * np.pi)
@@ -88,6 +91,7 @@ class GeneralOneQubitGate(OneQubitGate):
 
 class NMROneQubitGate(OneQubitGate):
     """One qubit gate limited to rotations in XY plane"""
+
     def __init__(self, params: np.ndarray = None):
         super().__init__(2, params)  # params: theta, phi
 
@@ -99,7 +103,7 @@ class NMROneQubitGate(OneQubitGate):
         d_theta = -1 / 2 * np.sin(self.params[0] / 2) * self._id - 1j / 2 * np.cos(self.params[0] / 2) * (
                 np.cos(self.params[1]) * self._x + np.sin(self.params[1]) * self._y)
         d_phi = 1j * np.sin(self.params[0] / 2) * (
-                np.sin(self.params[1]) * self._x + np.cos(self.params[1]) * self._y)
+                np.sin(self.params[1]) * self._x - np.cos(self.params[1]) * self._y)
         self.derivative[0] = d_theta
         self.derivative[1] = d_phi
 

GRAPE/optimizer.py
@@ -1,12 +1,13 @@
 import numpy as np
+
 from .circuit import Circuit, OneQubitEntanglementAlternation
-from .multiqubitgates import CXCascade
+from .multiqubitgates import Evolution
 
 
 class GradientOptimization:
     def __init__(self, target, circuit: Circuit = None, filename: str = None):
         self.target = target  # unitary to approximate
-        self.stepSize = 0.1  # gradient-to-change ration
+        self.learning_rate = 0.01  # gradient-to-change ration
 
         if filename is not None:
             raise NotImplementedError
@@ -16,10 +17,11 @@ class GradientOptimization:
             self.phase = 0  # global phase
 
             if circuit is None:
-                self.circuit = OneQubitEntanglementAlternation(self._size, entanglement_gate_type=CXCascade,
+                self.circuit = OneQubitEntanglementAlternation(self._size, entanglement_gate_type=Evolution,
                                                                number_of_entanglements=2 * self._size)
             else:
                 self.circuit = circuit
+        self.set_hamiltonian = self.circuit.set_hamiltonian
         self.update()
 
     @property
@@ -54,8 +56,7 @@ class GradientOptimization:
         :return: derivative of norm
         """
         return (((self.matrix - self.target) @ derivative.conjugate().T).trace() + (
-                derivative @ (
-                    self.matrix - self.target).conjugate().T).trace()).real
+                derivative @ (self.matrix - self.target).conjugate().T).trace()).real
 
     def update(self):
         """Update the circuit"""
@@ -69,34 +70,24 @@ class GradientOptimization:
         for i in range(len(self.circuit.gates)):
             for parameter in range(len(self.circuit.gates[i].params)):
                 metric_derivative = self.metric_derivative(self.circuit.derivative(i, parameter))
-                self.circuit.gates[i].params[parameter] -= self.stepSize * metric_derivative
+                self.circuit.gates[i].params[parameter] -= self.learning_rate * metric_derivative
 
     def descend(self, steps=1000, track_distance=False):
         distances = []  # distances to track
 
+        self.update()
         for i in range(steps):
             distances.append(self.metric_distance)
             self.corrections_from_gradients()
             self.update()
-            self.phase -= np.angle((self.target_d @ self.matrix).trace())
+            self.phase = np.angle((self.target_d @ self.matrix).trace())
 
         # most parameters are cyclic - make them in (0, max)
         self.circuit.normalize()
-
-        self.phase = self.phase.real % (2 * np.pi)
+        self.update()
 
         if track_distance:
             return distances
-
-    def to_qiskit(self):
-        raise NotImplementedError
-        # return self.circuit.to_quiskit()
-
-    def set_j(self, new_j):
-        raise NotImplementedError
-        # for gate in self.gates:
-        #     if type(gate) is Delay:
-        #         gate.set_j(new_j)
 
     def make_times_positive(self):
         raise NotImplementedError

GRAPE/profilerGRAPE.py
@@ -1,7 +1,8 @@
 import numpy as np
-from GRAPE import GradientOptimization
-from circuit import Circuit
-from multiqubitgates import Pulse, Delay
+
+from GRAPE.circuit import Circuit
+from GRAPE.multiqubitgates import Pulse, Evolution
+from GRAPE.optimizer import GradientOptimization
 
 cx = np.asarray([[1, 0, 0, 0],
                  [0, 1, 0, 0],
@@ -19,13 +20,12 @@ toffoli = np.asarray([[1, 0, 0, 0, 0, 0, 0, 0],
 
 circuit = Circuit(3)
 circuit += Pulse(3, "nmr")
-circuit += Delay(3)
+circuit += Evolution(3)
 circuit += Pulse(3, "nmr")
-circuit += Delay(3)
+circuit += Evolution(3)
 circuit += Pulse(3, "nmr")
-circuit += Delay(3)
+circuit += Evolution(3)
 circuit += Pulse(3, "nmr")
-
 
 desc = GradientOptimization(toffoli, circuit)
 desc.randomize_params()

GRAPE/tests/test_circuit.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
 
 import numpy as np
+
 from GRAPE.circuit import Circuit
-from GRAPE.multiqubitgates import Pulse
+from GRAPE.multiqubitgates import Evolution, Pulse, CXCascade
 
 
 class TestCircuit(TestCase):
@@ -44,13 +45,23 @@ class TestCircuit(TestCase):
 
     def test_derivative(self):
         circuit = Circuit(2)
-        circuit += Pulse(2)
-        circuit += Pulse(2)
+        circuit += Pulse(2, one_qubit_gate_type="general")
+        circuit += Evolution(2)
+        circuit += CXCascade(2)
+        circuit += Pulse(2, one_qubit_gate_type="nmr")
+        circuit += Evolution(2)
         circuit += Pulse(2)
 
         circuit.randomize_params()
         circuit.update()
-        for parameter in range(len(circuit.gates[2].params)):
-            np.testing.assert_allclose(
-                circuit.gates[2].derivative[parameter] @ circuit.gates[1].matrix @ circuit.gates[0].matrix,
-                circuit.derivative(2, parameter))
+
+        test_increment = 0.0001
+        matrix = circuit.matrix
+        for i in range(len(circuit.gates)):
+            for param in range(len(circuit.gates[i].params)):
+                derivative = circuit.derivative(i, param)
+                circuit.gates[i].params[param] += test_increment
+                circuit.update()
+                np.testing.assert_allclose((circuit.matrix - matrix) / test_increment, derivative,
+                                           rtol=0.01)
+                circuit.gates[i].params[param] -= test_increment

GRAPE/tests/test_multiqubitgates.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
 
 import numpy as np
-from GRAPE.multiqubitgates import Delay, Pulse, Inversion, CXCascade
+
+from GRAPE.multiqubitgates import Evolution, Pulse, Inversion, CXCascade
 from GRAPE.onequbitgates import GeneralOneQubitGate
 
 
@@ -34,6 +35,36 @@ class TestPulse(TestCase):
             np.testing.assert_allclose(pulse.derivative[pulse.basic_gates[0].number_of_parameters + i],
                                        np.kron(pulse.basic_gates[1].derivative[i], pulse.basic_gates[0].matrix),
                                        rtol=0.001, atol=0.001)
+
+    def test_derivative(self):
+        pulse = Pulse(2)
+        pulse.randomize_params()
+        pulse.update()
+
+        test_increment = 0.0001
+        derivative = pulse.derivative
+        matrix = pulse.matrix
+        for param in range(len(pulse.params)):
+            pulse.params[param] += test_increment
+            pulse.update()
+            np.testing.assert_allclose((pulse.matrix - matrix) / test_increment, derivative[param],
+                                       rtol=0.01)
+            pulse.params[param] -= test_increment
+
+    def test_derivative_nmr(self):
+        pulse = Pulse(2, one_qubit_gate_type="nmr")
+        pulse.randomize_params()
+        pulse.update()
+
+        test_increment = 0.0001
+        derivative = pulse.derivative
+        matrix = pulse.matrix
+        for param in range(len(pulse.params)):
+            pulse.params[param] += test_increment
+            pulse.update()
+            np.testing.assert_allclose((pulse.matrix - matrix) / test_increment, derivative[param],
+                                       rtol=0.01)
+            pulse.params[param] -= test_increment
 
     def test_to_circuit(self):
         pulse = Pulse(2)
@@ -105,9 +136,24 @@ class TestCXCascade(TestCase):
 
 class TestDelay(TestCase):
     def test_params(self):
-        delay = Delay(2)
+        delay = Evolution(2)
         for i in range(len(delay.params)):
             old = delay.params[i]
             delay.params[i] = delay.params[i]
             new = delay.params[i]
             self.assertEqual(old, new)
+
+    def test_derivative(self):
+        delay = Evolution(2)
+        delay.randomize_params()
+        delay.update()
+
+        test_increment = 0.0001
+        derivative = delay.derivative
+        matrix = delay.matrix
+        for param in range(len(delay.params)):
+            delay.params[param] += test_increment
+            delay.update()
+            np.testing.assert_allclose((delay.matrix - matrix) / test_increment, derivative[param],
+                                       rtol=0.01)
+            delay.params[param] -= test_increment

GRAPE/tests/test_onequbitgates.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 import numpy as np
+
 from GRAPE.onequbitgates import GeneralOneQubitGate, NMROneQubitGate
 
 
@@ -19,27 +20,6 @@ class TestGeneralOneQubitGate(TestCase):
         gate.update_matrix()
         np.testing.assert_allclose(np.asarray([[1, 0],
                                                [0, 1]], dtype=complex), gate.matrix, rtol=0.001, atol=0.001)
-
-    def test_update_derivative(self):
-        gate = GeneralOneQubitGate()
-
-        gate.params = np.asarray([np.pi, 0, 0], dtype=float)
-        gate.update_derivative()
-        np.testing.assert_allclose(np.asarray([[-1 / 2, 0],
-                                               [0, -1 / 2]], dtype=complex), gate.derivative[0], rtol=0.001, atol=0.001)
-        np.testing.assert_allclose(np.asarray([[0, 0],
-                                               [1j, 0]], dtype=complex), gate.derivative[1], rtol=0.001, atol=0.001)
-        np.testing.assert_allclose(np.asarray([[0, -1j],
-                                               [0, 0]], dtype=complex), gate.derivative[2], rtol=0.001, atol=0.001)
-
-        gate.params = np.asarray([0, 0, 0], dtype=float)
-        gate.update_derivative()
-        np.testing.assert_allclose(np.asarray([[0, -1 / 2],
-                                               [1 / 2, 0]], dtype=complex), gate.derivative[0], rtol=0.001, atol=0.001)
-        np.testing.assert_allclose(np.asarray([[0, 0],
-                                               [0, 1j]], dtype=complex), gate.derivative[1], rtol=0.001, atol=0.001)
-        np.testing.assert_allclose(np.asarray([[0, 0],
-                                               [0, 1j]], dtype=complex), gate.derivative[1], rtol=0.001, atol=0.001)
 
     def test_randomize_params(self):
         gate = GeneralOneQubitGate()
@@ -66,11 +46,26 @@ class TestGeneralOneQubitGate(TestCase):
         np.testing.assert_allclose(gate.matrix @ gate.matrix.T.conjugate(), np.eye(2, dtype=complex), rtol=0.001,
                                    atol=0.001)
 
+    def test_derivative(self):
+        gate = GeneralOneQubitGate()
+        gate.randomize_params()
+        gate.update()
+
+        test_increment = 0.0001
+        derivative = gate.derivative
+        matrix = gate.matrix
+        for param in range(len(gate.params)):
+            gate.params[param] += test_increment
+            gate.update()
+            np.testing.assert_allclose((gate.matrix - matrix) / test_increment, derivative[param],
+                                       rtol=0.01)
+            gate.params[param] -= test_increment
+
+
 class TestNMROneQubitGate(TestCase):
     def test_default_params(self):
         gate = NMROneQubitGate()
         np.testing.assert_allclose(gate.params, np.asarray([0, 0], dtype=float), rtol=0.001, atol=0.001)
-
 
     def test_update_matrix(self):
         gate = NMROneQubitGate()
@@ -85,31 +80,6 @@ class TestNMROneQubitGate(TestCase):
         np.testing.assert_allclose(np.asarray([[1, 0],
                                                [0, 1]], dtype=complex), gate.matrix, rtol=0.001, atol=0.001)
 
-
-    def test_update_derivative(self):
-        gate = NMROneQubitGate()
-
-        gate.params = np.asarray([np.pi, 0], dtype=float)
-        gate.update_derivative()
-        np.testing.assert_allclose(np.asarray([[-1 / 2, 0],
-                                               [0, -1 / 2]], dtype=complex), gate.derivative[0], rtol=0.001, atol=0.001)
-
-        gate.params = np.asarray([0, 0], dtype=float)
-        gate.update_derivative()
-        np.testing.assert_allclose(np.asarray([[0, -0.5j],
-                                               [-0.5j, 0]], dtype=complex), gate.derivative[0], rtol=0.001, atol=0.001)
-
-        gate.params = np.asarray([np.pi, 0], dtype=float)
-        gate.update_derivative()
-        np.testing.assert_allclose(np.asarray([[0, 1],
-                                               [-1, 0]], dtype=complex), gate.derivative[1], rtol=0.001, atol=0.001)
-
-        gate.params = np.asarray([0, 0], dtype=float)
-        gate.update_derivative()
-        np.testing.assert_allclose(np.asarray([[0, 0],
-                                               [0, 0]], dtype=complex), gate.derivative[1], rtol=0.001, atol=0.001)
-
-
     def test_randomize_params(self):
         gate = NMROneQubitGate()
         gate.randomize_params()
@@ -117,7 +87,6 @@ class TestNMROneQubitGate(TestCase):
             self.assertEqual(type(p), np.float_)
             self.assertGreaterEqual(p, 0)
             self.assertLessEqual(p, 2 * np.pi)
-
 
     def test_normalize(self):
         gate = NMROneQubitGate()
@@ -129,7 +98,6 @@ class TestNMROneQubitGate(TestCase):
         matrix2 = gate.matrix
         np.testing.assert_allclose(matrix1, matrix2)
 
-
     def test_unitarity(self):
         gate = GeneralOneQubitGate()
         gate.randomize_params()
@@ -137,4 +105,17 @@ class TestNMROneQubitGate(TestCase):
         np.testing.assert_allclose(gate.matrix @ gate.matrix.T.conjugate(), np.eye(2, dtype=complex), rtol=0.001,
                                    atol=0.001)
 
+    def test_derivative(self):
+        gate = NMROneQubitGate()
+        gate.randomize_params()
+        gate.update()
 
+        test_increment = 0.0001
+        derivative = gate.derivative
+        matrix = gate.matrix
+        for param in range(len(gate.params)):
+            gate.params[param] += test_increment
+            gate.update()
+            np.testing.assert_allclose((gate.matrix - matrix) / test_increment, derivative[param],
+                                       rtol=0.01)
+            gate.params[param] -= test_increment